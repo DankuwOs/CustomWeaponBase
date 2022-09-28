60e38a220e35f3dfedc614b1d52bdbd84db7aa5f
DankuwOs <infamousII@hotmail.com>
2022-09-27 22:18:48 -0500

    ops didnt put it in

---
+++ CustomWeaponBase/Patches/Patch_HPEquipOML.cs
@@ -0,0 +1,17 @@
+﻿
+using Harmony;
+using UnityEngine;
+
+[HarmonyPatch(typeof(HPEquipOpticalML), nameof(HPEquipOpticalML.GetCount))]
+public class Patch_HPEquipOML
+{
+    public static bool Prefix(HPEquipOpticalML __instance)
+    {
+        if (!__instance.oml)
+        {
+            __instance.oml = __instance.ml as OpticalMissileLauncher; // Fixes issue in mp where getcount doesnt know what the fuck an oml is
+        }
+
+        return true;
+    }
+}
---
+++ CustomWeaponBase/Patches/Patch_SMSInternalWhateverTheFuck.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace CustomWeaponBase.Patches;
+
+[HarmonyPatch(typeof(SMSInternalWeaponAnimator), nameof(SMSInternalWeaponAnimator.UpdateCurrentProfile))]
+public class Patch_SMSInternalWhateverTheFuck
+{
+    public static bool Prefix(SMSInternalWeaponAnimator __instance, ref HPEquippable ___eq)
+    {
+        return !___eq.GetComponent<CWB_Weapon>();
+    }
+}
