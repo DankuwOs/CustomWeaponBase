80a81f11d858f0a5e79d1cde1d45054fad44c9c4
DankuwOs <infamousII@hotmail.com>
2022-09-28 13:21:36 -0500

    Airburst missile & FLKMissile (tbused with airburst) added.

---
+++ CustomWeaponBase/Custom Components/BurstMissile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BurstMissile : Missile
+{
+    [Header("Burst")]
+    public float distance;
+
+    [Tooltip("Event invoked when missile enters distance ↑")]
+    public UnityEvent OnBurst = new UnityEvent();
+
+    public override void Fire()
+    {
+        base.Fire();
+
+        StartCoroutine(BurstMissileRoutine());
+    }
+
+    private IEnumerator BurstMissileRoutine()
+    {
+        while (lastTargetDistance > distance)
+        {
+            yield return null;
+        }
+        OnBurst.Invoke();
+    }
+}
---
+++ CustomWeaponBase/Custom Components/FLKMissile.cs
@@ -0,0 +1,64 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using VTNetworking;
+using VTOLVR.Multiplayer;
+
+public class FLKMissile : MonoBehaviour
+{
+    public Missile missile;
+    
+    [Header("Optional")]
+    public MissileFairing[] fairings;
+
+    [Header("Optional")]
+    public Gun gun;
+
+    public float fireDelay;
+    
+    [Tooltip("Invoked before gun starts firing")]
+    public UnityEvent OnFire = new UnityEvent();
+
+    public UnityEvent OnEmpty = new UnityEvent();
+    
+    public void StartFLKRoutine()
+    {
+        if (VTOLMPUtils.IsMultiplayer())
+        {
+            VTNetEntity component = base.GetComponent<VTNetEntity>();
+            if (component && !component.isMine)
+            {
+                return;
+            }
+        }
+
+        StartCoroutine(FLKRoutinte());
+    }
+
+    private IEnumerator FLKRoutinte()
+    {
+        gun.actor = missile.actor;
+        
+        if (fairings.Length > 0)
+            foreach (var missileFairing in fairings)
+            {
+                missileFairing.Jettison();
+            }
+
+        yield return new WaitForSeconds(fireDelay);
+        
+        OnFire.Invoke();
+        if (!gun)
+            yield break;
+        
+        gun.SetFire(true);
+
+        while (gun.currentAmmo > 0)
+        {
+            yield return null;
+        }
+        
+        OnEmpty.Invoke();
+    }
+}
