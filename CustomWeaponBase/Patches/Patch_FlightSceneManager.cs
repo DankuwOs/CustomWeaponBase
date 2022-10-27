8d9079317ee7995fdb5b00bc8b348651b2d6d116
DankuwOs <infamousII@hotmail.com>
2022-10-27 03:06:13 -0400

    ghfhg

---
+++ CustomWeaponBase/Patches/Patch_FlightSceneManager.cs
@@ -0,0 +1,13 @@
+﻿using Harmony;
+
+[HarmonyPatch(typeof(FlightSceneManager), nameof(FlightSceneManager.Awake))]
+public class Patch_FlightSceneManager
+{
+    public static void Postfix()
+    {
+        if (CustomWeaponsBase.instance)
+        {
+            FlightSceneManager.instance.OnExitScene += CustomWeaponsBase.instance.DestroyDetachedObjects; // Don't know if FSM is ever destroyed so doing this..
+        }
+    }
+}
---
+++ CustomWeaponBase/Patches/Patch_Missile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+[HarmonyPatch(typeof(Missile), nameof(Missile.Detonate), new Type[] { typeof(Collider) })]
+public class Patch_Missile
+{
+    public static void Postfix(Missile __instance, Vector3 ___explosionNormal)
+    {
+        var cwbExp = __instance.GetComponent<CWB_Explosion>();
+        if (!cwbExp)
+            return;
+
+
+        foreach (var explosion in cwbExp.explosions) 
+        {
+            var scale = __instance.transform.localScale.x + __instance.transform.localScale.y + __instance.transform.localScale.z / 3;
+            cwbExp.CreateExplosionEffect(explosion, __instance.transform.position, cwbExp.useNormal? ___explosionNormal : Vector3.up, scale);
+        }
+        if (!FlybyCameraMFDPage.instance || !FlybyCameraMFDPage.instance.isCamEnabled || !FlybyCameraMFDPage.instance.flybyCam || cwbExp.shake < 0.05f) return;
+        float num2 = cwbExp.shake * cwbExp.shake / (FlybyCameraMFDPage.instance.flybyCam.transform.position - __instance.transform.position).sqrMagnitude;
+        FlybyCameraMFDPage.instance.ShakeCamera(num2 * 2f);
+    }
+}
