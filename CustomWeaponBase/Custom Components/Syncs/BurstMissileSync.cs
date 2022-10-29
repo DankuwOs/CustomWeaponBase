6beabaacc6910ead2ab99b8d2bf710c17f1585f8
DankuwOs <infamousII@hotmail.com>
2022-10-29 13:27:39 -0400

    oops

---
+++ CustomWeaponBase/Custom Components/Syncs/BurstMissileSync.cs
@@ -0,0 +1,30 @@
+﻿
+using VTNetworking;
+
+public class BurstMissileSync : VTNetSyncRPCOnly
+{
+    public BurstMissile burstMissile;
+    
+    protected override void OnNetInitialized()
+    {
+        base.OnNetInitialized();
+        if (netEntity == null)
+            return;
+        
+        burstMissile.OnBurst.AddListener(OnBurst);
+    }
+
+    private void OnBurst()
+    {
+        if (!isMine)
+            return;
+        
+        SendRPC("BurstMissile_Burst");
+    }
+
+    [VTRPC]
+    private void BurstMissile_Burst()
+    {
+        burstMissile.OnBurst.Invoke();
+    }
+}
---
+++ CustomWeaponBase/Custom Components/Syncs/FLKMissileSync.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine.InputSystem.Utilities;
+using VTNetworking;
+
+public class FLKMissileSync : VTNetSyncRPCOnly
+{
+    public FLKMissile flkMissile;
+    
+    protected override void OnNetInitialized()
+    {
+        base.OnNetInitialized();
+        if (netEntity == null)
+            return;
+        
+        flkMissile.OnFire.AddListener(OnFire);
+        flkMissile.OnEmpty.AddListener(OnEmpty);
+    }
+
+    private void OnFire()
+    {
+        if (!isMine)
+            return;
+        
+        SendRPC("FLKM_OnFire");
+    }
+    
+    private void OnEmpty()
+    {
+        if (!isMine)
+            return;
+        
+        SendRPC("FLKM_OnEmpty");
+    }
+
+    [VTRPC]
+    private void FLKM_OnFire()
+    {
+        flkMissile.OnFire.Invoke();
+    }
+    
+    [VTRPC]
+    private void FLKM_OnEmpty()
+    {
+        flkMissile.OnEmpty.Invoke();
+    }
+}
